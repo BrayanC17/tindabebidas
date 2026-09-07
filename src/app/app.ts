94176c7a23da3656c6db9c193fa9609ba569951d
BrayanC17 <bcastrovarela@ucompensar.edu.co>
2026-09-07 01:17:46 -0500

    beggin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Bebidas } from './components/bebidas/bebidas';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Bebidas],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('tindabebidas');
+  nameproyecto = signal('tindabebidas');
+  description = signal('Proyecto de Angular 17 con Vite y TypeScript 6.0.3');
+  autor = signal('Brayan C');
 }
